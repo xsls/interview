676c17410bc8d4d339cb9922736b443603b1f67a
nieyulin112 <1139723906@qq.com>
2016-06-22 08:54:10 +0800

    add: 经验的总结

---
+++ es6/es6.js
@@ -0,0 +1,63 @@
+// js基本的面向对象的编程
+class Point {
+	// 构造函数的使用
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	};
+	toString() {
+		return this.x + '.....' + this.y;
+	}
+}
+
+// 1.继承的关键字: extends
+// 2.构造函数: constructor
+class SuperPoint extends Point {
+	constructor(x, y, color) {
+		// 继承起方法
+		super(x,y);
+		// this.x = x;
+		// this.y = y;
+		this.color = color;
+	};
+	toString() {
+		return this.color + '........' + super.toString(); 
+	}
+}
+
+let childPoint = new SuperPoint(1, 2, 'red');
+
+console.log(childPoint.toString());
+
+
+// generator
+function* generator () {
+	yield 'hello';
+  	yield 'world';
+  	return 'ending';
+}
+
+var g = generator();
+
+console.log(g.next());
+console.log(g.next());
+console.log(g.next());
+console.log(g.next());
+console.log(g.next());
+console.log(g.next());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
