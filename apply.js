e5940e1202524a4dc30e130666daae9b23a11de0
nieyulin112 <1139723906@qq.com>
2016-07-18 14:50:24 +0800

    缓存机制的总结

--- apply.js
+++ apply.js
@@ -1,3 +1,19 @@
 apply的数据的应用,提供数据
 
 apply传参数和call的区别
+
+
+第一个参数都是对象
+
+
+例子：
+var slice ＝ Array.prototype.slice;
+
+console.log('slice', slice);
+
+var arr = [1,2,3];
+
+arr.slice(0,1);
+
+slice.call(arr, 0, 1);
+slice.apply(arr, [0,1]);
