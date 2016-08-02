4ad9996527af977adf34a6361a2ac1f5f738a190
nieyulin112 <1139723906@qq.com>
2016-08-02 11:06:06 +0800

    feat_interview:mongodb

--- node/index.js
+++ node/index.js
@@ -1,10 +1,41 @@
+const PORT = 3000;
 var http = require('http');
-// var path = require('path');
-var server = http.createServer(function(req, res) {
-    res.end();
-}).listen(1377, '127.0.0.1');
-server.setTimeout(5000, function(socket) {
-    console.log('服务器请求超时');
-    console.log('socket', socket);
+var url=require('url');
+var fs=require('fs');
+var path=require('path');
 
-})
+var server = http.createServer(function (request, response) {
+    var pathname = url.parse(request.url).pathname;
+    var realPath = path.join("assets", pathname);
+    //console.log(realPath);
+    var ext = path.extname(realPath);
+    ext = ext ? ext.slice(1) : 'unknown';
+    fs.exists(realPath, function (exists) {
+        if (!exists) {
+            response.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
+
+            response.write("This request URL " + pathname + " was not found on this server.");
+            response.end();
+        } else {
+            fs.readFile(realPath, "binary", function (err, file) {
+                if (err) {
+                    response.writeHead(500, {
+                        'Content-Type': 'text/plain'
+                    });
+                    response.end(err);
+                } else {
+                    var contentType = mine[ext] || "text/plain";
+                    response.writeHead(200, {
+                        'Content-Type': contentType
+                    });
+                    response.write(file, "binary");
+                    response.end();
+                }
+            });
+        }
+    });
+});
+server.listen(PORT);
+console.log("Server runing at port: " + PORT + ".");
