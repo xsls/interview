ff3536c47ff6a53662e8bf9852e95a1b9c0ded6a
nieyulin112 <1139723906@qq.com>
2016-08-19 11:01:32 +0800

    文件的修改

--- node/mongodb.js
+++ node/mongodb.js
@@ -1,8 +1,20 @@
 // 启动服务:sudo mongod
 // 查看数据库:mongo
 // 查看更多数据
+// 配置文件的基本路径:/usr/local/var/log
+// systemLog:
+//   destination: file
+//   path: /usr/local/var/log/mongodb/mongo.log
+//   logAppend: true
+// storage:
+//   dbPath: /usr/local/var/mongodb
+// net:
+//   bindIp: 127.0.0.1
+// 配置文件及端口的配置:
+// /usr/local/etc/mongod.conf 
 const mongo = require('mongodb');
 const host = 'localhost';
+// 配置端口号的使用
 const port = 27017;
 const server = new mongo.Server(host, port, {auto_reconnect: true});
 const db = new mongo.Db('database', server, {safe: true});
