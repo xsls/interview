4206db71bb1d8de260920a454a691237d2c7831a
nieyulin112 <1139723906@qq.com>
2016-08-29 20:35:48 +0800

    算法

--- js/js权威指南.js
+++ js/js权威指南.js
@@ -3,3 +3,12 @@ js
 css
 html
 基础框架
+
+删除数组中对象的方法
+Array.prototye.remove = function (obj) {
+    const index = this.indexOf(obj);
+    if (index === -1) {
+        return ;
+    }
+    this.splice(index, 1);
+};
